b5a3e63c45f35bd2e48cd72a4f155e3ddf57d747
cikaricIvan <cikaricivan@yahoo.com>
2023-02-21 20:25:45 +0100

    proba

---
+++ src/main/java/com/iktpreobuka/my_repo_sts/ExampleMyRepoSts1Application.java
@@ -0,0 +1,13 @@
+package com.iktpreobuka.my_repo_sts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleMyRepoSts1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleMyRepoSts1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/iktpreobuka/my_repo_sts/ExampleMyRepoSts1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iktpreobuka.my_repo_sts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleMyRepoSts1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
